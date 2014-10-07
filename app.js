8d1968ee101b590d604e2336720cb3f1f4f98599
roni <roni@applicat.com>
2014-10-07 13:01:16 +0300

    added drop correction indication.

--- app.js
+++ app.js
@@ -1,24 +1,47 @@
-function MainController(){
-    var vm=this;
-    vm.hello ='roni';
-}
-function allowDrop(ev) {
-    ev.preventDefault();
-}
+(function () {
+    function MainController(){
+        var vm=this;
+        vm.hello ='roni';
+    }
+    angular.module('dragAndDrop',[])
+        .controller({
+            MainController:MainController
+        })
+        .directive('droppableArea', function() {
+            return {
+                restrict: 'A', //attribute only
+                scope: true,
+                link: function(scope, elem, attr, ctrl) {
+                    scope.dropped=false;
+                    elem.bind('drop', function(ev) {
+                        ev.preventDefault();
+                        var data = ev.dataTransfer.getData("text/html");
+                        ev.target.appendChild(document.getElementById(data));
+                        if (data === elem[0].id){
+                            scope.correct=true;
+                            scope.dropped=true;
+                            scope.$digest();
+                        } else {
+                            scope.correct=false;
+                            scope.dropped=true;
+                            scope.$digest();
+                        }
 
-function drag(ev) {
-    ev.dataTransfer.setData("text/html", ev.target.id);
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text/html");
-    ev.target.appendChild(document.getElementById(data));
-}
-
-angular.module('dragAndDrop',[])
-    .controller({
-        MainController:MainController
-    })
-
-
+                    });
+                    elem.bind('dragover', function(ev) {
+                        ev.preventDefault();
+                    });
+                }
+            };
+        })
+        .directive('draggableThing', function(){
+            return {
+                restrict: 'A', //attribute only
+                link: function(scope, elem, attr, ctrl) {
+                    elem.bind('dragstart', function(ev) {
+                        ev.dataTransfer.setData("text/html", ev.target.id);
+                    });
+                }
+            };
+        });
+}());
