1a9ca64afff194bb201bba48f2b284f470edc2a3
Roni Hacohen <ronihcohen@gmail.com>
2014-10-19 20:19:14 +0300

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function (grunt) {
         options: {
           jshintrc: '.jshintrc'
         },
-        src: 'src/perfect-scrollbar.js'
+        src: 'app.js'
       }
     }
   });
